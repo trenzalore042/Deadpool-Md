c55864ee954473472d99b1d07f2fa0a484edcc86
trenzalore042 <144277261+trenzalore042@users.noreply.github.com>
2024-06-19 16:57:21 +0100

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -18,16 +18,16 @@ Base Haikal Hw Mods : Copyan Dari Base Dika
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.region = 'kenya' // serah
+global.region = 'Nigeria' // serah
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = '𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'TRENZALORE' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
-global.owner = ['254796283064'] // ubah aja pake nomor lu
+global.owner = ['2348168855791'] // ubah aja pake nomor lu
 //==========================zetsuboclient=======================//
-global.botname = '𝗗𝗘𝗔𝗗𝗣𝗢𝗢𝗟 𝗕𝗨𝗚' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.botname = 'CYBER_X_3.0V 𝗕𝗨𝗚' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.packname = '𝘾𝙤𝙣𝙛𝙧𝙤𝙣𝙩𝙚𝙧' // ubah aja ini nama sticker
-global.author = '𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁' // ubah aja ini nama sticker
-global.prefa = ['','!','.',',','🐤','🗿']
+global.author = 'SILENT' // ubah aja ini nama sticker
+global.prefa = [',']
 global.sessionName = 'classicsession' //Gausah Juga
 global.sp = '⭔' // Gausah Juga
 global.wlcm = []
